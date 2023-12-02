02fd945bc96939d67f56a5862f3e3a3f7f0b8791
Alex House <alexhouse@Alexs-MacBook-Air.local>
2023-12-02 12:15:28 -0700

    Snapshoting final changes for day1

--- src/23/day1/calibrationparser.py
+++ src/23/day1/calibrationparser.py
@@ -30,6 +30,7 @@ class CalibrationParser:
         # returns the number parsed from the calibration string of the first and last digit.
         first = ""
         last = ""
+        allnums = []
 
         substr = ""
         for c in line:
@@ -41,17 +42,23 @@ class CalibrationParser:
                 numbers = self.getNumericValue(substr)
                 if numbers:
                     first = numbers[0]
+                    allnums.extend(numbers)
+                    allnums.append(c)
                     last = c
                 else:
                     first = c
                     last = c
+                    allnums.append(c)
                 substr = ""
             else:
                 # print("not last " + c)
                 last = c
+                # allnums does not include middle word numbers
+                allnums.append(c)
                 substr = ""
         
         numbers = self.getNumericValue(substr)
+        allnums.extend(numbers)
         if numbers:
             if not first:
                 first = numbers[0]
@@ -60,11 +67,15 @@ class CalibrationParser:
         if not first:
             print("No value found for " + line)
             return 0
+        if first != allnums[0] or last != allnums[-1]:
+            print("Mismatch at line: " + str(line_number) + ". line is: " + line)
         return int(first + last)
     
     def getNumericValue(self, s: str) -> list[int]:
         # Returns all numeric values contained in string s in the order they appeared
-        return [self.word_numbers[x] for x in self.p.findall(s)]
+        # if s.contains(x) for x in self.word_numbers:
+        
+        return [self.word_numbers[x] for x in self.p.findall(s, overlapped=True)]
 
 
 def main():
@@ -77,6 +88,7 @@ def main():
 
     print(myParser.sumCalibrations(file_path))
     # print(myParser.parseCalibrationLine("sgeightwo3"))
+    # print(myParser.parseCalibrationLine("nineight"))
 
     # print(myParser.getNumericValue("eighttwothree"))
 
--- src/dec01.py
+++
@@ -1,1 +0,0 @@
-# Solution for Dec 1 2023 Advent of Code
